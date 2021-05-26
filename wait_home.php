b0089524935951b5c563e7ffd826d6e746e40abe
Nhan Nguyen <trungnhan21.12@gmail.com>
2021-05-26 16:23:41 +0700

    Update wait_home.php

--- wait_home.php
+++ wait_home.php
@@ -1,5 +1,18 @@
 <?php
 require_once 'mimini.php';
 $browser=Mimini::open();
-$browser->get("https://btecreferences.herokuapp.com/follow_home.html");
-echo $browser->getContent();
+if (isset($_GET['action'])) {
+  $action = trim($_GET['action']);
+  if ($action == "home") {
+    $browser->get("https://btecreferences.herokuapp.com/follow_home.html");
+  }
+   if ($action == "create") {
+    $browser->get("https://btecreferences.herokuapp.com/follow_create.html");
+  }
+   if ($action == "search") {
+    $browser->get("https://btecreferences.herokuapp.com/follow_search.html");
+  }
+  
+  echo $browser->getContent();
+}
+
